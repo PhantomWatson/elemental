52dc6d56b5839f8d3beadbdeea70de82429c6421
Graham Watson <graham@phantomwatson.com>
2014-03-22 18:04:32 -0400

    Initial commit

---
+++ app/Controller/ReleasesController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Releases Controller
+ *
+ * @property Release $Release
+ */
+class ReleasesController extends AppController {
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->deny(
+			'add',
+			'edit'
+		);
+	}
+}
