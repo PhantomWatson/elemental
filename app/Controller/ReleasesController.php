b2b64a07171ca2c0f9f458549bd260c866997963
Graham Watson <graham@phantomwatson.com>
2014-03-27 22:36:38 -0400

    Refactor add()

--- app/Controller/ReleasesController.php
+++ app/Controller/ReleasesController.php
@@ -16,7 +16,11 @@ class ReleasesController extends AppController {
 			$course_id = $this->request->course_id;
 			$this->loadModel('Course');
 			if ($this->Course->exists($course_id)) {
-				$this->set('course_id', $course_id);
+				$user_id = $this->Auth->user('id');
+				$this->set(array(
+					'course_id' => $course_id,
+					'title_for_layout' => 'Release of Liability'
+				));
 			} else {
 				throw new NotFoundException('Invalid course specified');
 			}
@@ -26,20 +30,17 @@ class ReleasesController extends AppController {
 
 		if ($this->request->is('post')) {
 			$this->Release->create();
-			$this->request->data['Release']['user_id'] = $this->Auth->user('id');
+			$this->request->data['Release']['user_id'] = $user_id;
 			if ($this->Release->save($this->request->data)) {
 				$this->Flash->success('Your liability release has been submitted');
 				$this->redirect(array(
 					'controller' => 'courses',
 					'action' => 'register',
-					$this->request->data['Release']['course_id']
+					$course_id
 				));
 			} else {
 				$this->Flash->error('There was an error submitting your liability release');
 			}
 		}
-		$this->set(array(
-			'title_for_layout' => 'Release of Liability'
-		));
 	}
 }
