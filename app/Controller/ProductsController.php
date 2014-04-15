685c00646e5c78c7498f8739c6b5999cea78e2b9
Graham Watson <graham@phantomwatson.com>
2014-04-14 20:28:53 -0400

    Pass $can_access to review_materials view

--- app/Controller/ProductsController.php
+++ app/Controller/ProductsController.php
@@ -16,11 +16,11 @@ class ProductsController extends AppController {
 	public function review_materials() {
 		/* A trailing slash is required for /app/webroot/.htaccess to
 		 * correctly route the Vizi Player's (/app/webroot/vizi/review_materials/vizi.swf)
-		 * requests for files stored in /app/webroot/vizi/review_materials */ 
+		 * requests for files stored in /app/webroot/vizi/review_materials */
 		if ($this->request->url == 'review_materials') {
 			$this->redirect('/review_materials/');
 		}
-		
+
 		$logged_in = $this->Auth->loggedIn();
 		$product = $this->Product->getReviewMaterials();
 		$this->set(array(
@@ -34,6 +34,7 @@ class ProductsController extends AppController {
 			$this->loadModel('User');
 			$user_attended = $this->User->hasAttendedCourse($user_id);
 			$user_purchased = $this->User->hasPurchased($user_id, $product['Product']['id']);
+			$can_access = $this->User->canAccessReviewMaterials($user_id);
 
 			if ($user_attended && ! $user_purchased) {
 				$this->set('jwt', $this->Product->getJWT($product['Product']['id'], $user_id));
@@ -41,7 +42,8 @@ class ProductsController extends AppController {
 
 			$this->set(compact(
 				'user_attended',
-				'user_purchased'
+				'user_purchased',
+				'can_access'
 			));
 		}
 	}
@@ -54,23 +56,23 @@ class ProductsController extends AppController {
 
 	public function route() {
 		$url = urldecode($this->request->url);
-		
+
 		// Strip out leading /vizi if present
 		if(stripos($url, 'vizi/') === 0) {
 			$url = substr($url, 5);
 		}
-		
+
 		if ($this->Auth->loggedIn()) {
-			
+
 			// Get product info
 			$path_split = explode('/', $url);
 			$product = reset($path_split);
 			switch ($product) {
 				case 'review_materials':
 					$product_id = $this->Product->getReviewMaterialsId();
-					break;	
+					break;
 			}
-				
+
 			// Verify that the user is authorized to access this
 			$user_id = $this->Auth->user('id');
 			$this->loadModel('User');
@@ -84,7 +86,7 @@ class ProductsController extends AppController {
 				default:
 					throw new NotFoundException('Error: Unrecognized product');
 			}
-			
+
 			// Serve media
 			if ($can_access) {
 				$this->response->file(WWW_ROOT.'vizi/'.$url);
@@ -93,20 +95,20 @@ class ProductsController extends AppController {
 				$this->Flash->error('Sorry, your account does not have access to that product.');
 			}
 		} else {
-			$this->Flash->error('Please log in to view that product.');	
+			$this->Flash->error('Please log in to view that product.');
 		}
-		
+
 		$this->redirect('/');
 	}
 
 	public function instructor_training() {
 		/* A trailing slash is required for /app/webroot/.htaccess to
 		 * correctly route the Vizi Player's (/app/webroot/vizi/instructor_training/.../vizi.swf)
-		 * requests for files stored in /app/webroot/vizi/instructor_training */ 
+		 * requests for files stored in /app/webroot/vizi/instructor_training */
 		if ($this->request->url == 'instructor_training') {
 			$this->redirect('/instructor_training/');
 		}
-		
+
 		$user_id = $this->Auth->user('id');
 		$this->loadModel('User');
 		$this->set(array(
