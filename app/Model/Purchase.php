0ea8361d2ab83502b5de2a7f5a82b761f2525cfb
Graham Watson <graham@phantomwatson.com>
2014-05-17 20:18:54 -0400

    Add purchaseStudentReviewModule()

--- app/Model/Purchase.php
+++ app/Model/Purchase.php
@@ -96,4 +96,39 @@ class Purchase extends AppModel {
 
 		return true;
 	}
+
+	public function purchaseStudentReviewModule($seller_data, $order_id, $jwt_decoded) {
+		// Check for required sellerData
+		if (! isset($seller_data['user_id'])) {
+			throw new BadRequestException('User ID missing');
+		} elseif (! $this->User->exists($seller_data['user_id'])) {
+			throw new BadRequestException('User #'.$seller_data['user_id'].' not found');
+		} elseif (! isset($seller_data['product_id'])) {
+			throw new BadRequestException('Product ID missing');
+		}
+
+		// Remove cached information
+		if (isset($seller_data['product_id'])) {
+			$cache_key = 'hasPurchased('.$seller_data['user_id'].', '.$seller_data['product_id'].')';
+			Cache::delete($cache_key);
+		}
+
+		// Record purchase
+		$this->create(array(
+			'quantity' => 1,
+			'product_id' => $seller_data['product_id'],
+			'user_id' => $seller_data['user_id'],
+			'order_id' => $order_id,
+			'jwt' => serialize($jwt_decoded)
+		));
+		if (! $this->save()) {
+			throw new InternalErrorException('Purchase could not be saved');
+		}
+
+		// Clear relevant cache keys
+		$cache_key = 'getReviewMaterialsAccessExpiration('.$seller_data['user_id'].')';
+		Cache::delete($cache_key);
+
+		return true;
+	}
 }
