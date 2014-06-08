332e986bdd1efbf404e56306cfdec68b0d058885
Graham Watson <graham@phantomwatson.com>
2014-06-07 22:39:28 -0400

    Use Course's new validation rule instead of validating class size in controller

--- app/Controller/CoursesController.php
+++ app/Controller/CoursesController.php
@@ -211,14 +211,10 @@ class CoursesController extends AppController {
 
 		if ($this->request->is('post') || $this->request->is('put')) {
 
-			if ($this->request->data['Course']['max_participants'] < $class_list_count) {
-				$this->Course->validationErrors['max_participants'] = "Can't set class size any smaller than the number of participants already registered.";
-			}
-
 			$this->request->data['Course']['id'] = $id;
 			$this->Course->set($this->request->data);
 
-			if (empty($this->Course->validationErrors) && $this->Course->validates() && empty($this->Course->validationErrors)) {
+			if ($this->Course->validates() && empty($this->Course->validationErrors)) {
 				if ($this->Course->save()) {
 					$this->Flash->success('The course has been updated');
 					if ($waiting_list_count && $this->request->data['Course']['max_participants'] > $class_list_count) {
