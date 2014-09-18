eb674b635b5d0c4d1ea7328efb898c9b9c6be32a
Graham Watson <graham@phantomwatson.com>
2014-09-18 12:35:38 -0400

    Fix how instructors are granted access to some actions

--- app/Controller/CoursesController.php
+++ app/Controller/CoursesController.php
@@ -50,9 +50,12 @@ class CoursesController extends AppController {
 				$instructor_id = $this->Course->field('user_id');
 				if ($instructor_id == $user['id']) {
 					return true;
+				} else {
+					return parent::isAuthorized($user);
 				}
 			}
-			return parent::isAuthorized($user);
+
+			return true;
 		}
 		return parent::isAuthorized($user);
 	}
