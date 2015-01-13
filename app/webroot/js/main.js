f73ba55bf9c6b7189333f1d391795a42b9463466
Graham Watson <graham@phantomwatson.com>
2015-01-12 23:06:35 -0500

    Move JS from view to main.js

--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -360,3 +360,63 @@ var adminUserIndex = {
 		});
 	}
 };
+
+var studentReviewPurchase_student = {
+	init: function (params) {
+		var handler = StripeCheckout.configure({
+			key: params.key,
+			image: 'http://elementalprotection.org/img/star-256px-whitebg.png',
+			panelLabel: 'Continue (Total: {{amount}})',
+			token: function(token) {
+				var modal = $('#confirmation_modal');
+				modal.on('shown.bs.modal', function() {
+					var dialog = modal.find('.modal-dialog');
+					var initModalHeight = dialog.outerHeight();
+					dialog.css('margin-top', (window.screenY / 2) + initModalHeight);
+				});
+				modal.modal();
+				modal.find('.btn-primary').click(function (event) {
+					event.preventDefault();
+					modal.find('.btn').addClass('disabled');
+					var hangon = $('<p class=\"hangon\" style=\"display: none;\">Please wait... <img src=\"/img/loading_small.gif\" /></p>');
+					modal.find('.modal-body').append(hangon);
+					hangon.slideDown();
+					var data = {
+						student_id: params.user_id,
+						token: token.id
+					};
+					$.ajax({
+						type: 'POST',
+						url: '/student_review_modules/complete_student_purchase',
+						data: data,
+						success: function (data, textStatus, jqXHR) {
+							if (data.success) {
+								modal.modal('hide');
+								location.reload(true);
+							} else {
+								modal.find('.btn-primary').remove();
+								modal.find('.btn').removeClass('disabled');
+								modal.find('.modal-title').html('Error');
+								modal.find('.modal-body').html(data.message);
+							}
+						},
+						dataType: 'json'
+					});
+				});
+			}
+		});
+
+		$('#purchase_student_review').on('click', function(e) {
+			handler.open({
+				name: 'Elemental',
+				description: 'Review Module access renewal ($'+params.cost_dollars+')',
+				amount: params.cost_dollars * 100
+			});
+			e.preventDefault();
+		});
+
+		$(window).on('popstate', function() {
+			handler.close();
+		});
+	}
+};
