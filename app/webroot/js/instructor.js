06400a44182b9e56f08b03b55156caf6c0e0b3b4
Graham Watson <graham@phantomwatson.com>
2015-01-20 21:47:59 -0500

    Use new streamlined purchase button setup

--- app/webroot/js/instructor.js
+++ app/webroot/js/instructor.js
@@ -1,22 +1,5 @@
 var studentReviewPurchase_instructor = {
 	init: function (params) {
-		var handler = elementalPurchase.getStripeHandler({
-			key: params.key,
-			post_data: params.post_data,
-			post_url: params.post_url
-		});
-
-		$('#pay_outstanding').on('click', function(e) {
-			handler.open({
-				name: 'Elemental',
-				description: params.description,
-				amount: params.cost_dollars * 100
-			});
-			e.preventDefault();
-		});
-
-		$(window).on('popstate', function() {
-			handler.close();
-		});
+		elementalPurchase.setupPurchaseButton(params);
 	}
 };
