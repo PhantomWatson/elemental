e5ab5f94456cc80dfbe806ac8127a20dd6462047
Graham Watson <graham@phantomwatson.com>
2015-01-20 20:59:43 -0500

    Create object studentReviewPurchase_instructor

---
+++ app/webroot/js/instructor.js
@@ -0,0 +1,25 @@
+var studentReviewPurchase_instructor = {
+	init: function (params) {
+		var handler = elementalPurchase.getStripeHandler({
+			key: params.key,
+			post_data: {
+				instructor_id: params.instructor_id,
+				quantity: params.quantity
+			},
+			post_url: '/student_review_modules/complete_instructor_purchase'
+		});
+
+		$('#pay_outstanding').on('click', function(e) {
+			handler.open({
+				name: 'Elemental',
+				description: 'Pay for '+params.quantity+' Student Review Module(s) ($'+params.cost_dollars+')',
+				amount: params.cost_dollars * 100
+			});
+			e.preventDefault();
+		});
+
+		$(window).on('popstate', function() {
+			handler.close();
+		});
+	}
+};
