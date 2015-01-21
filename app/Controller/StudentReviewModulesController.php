96f85bb5e9ffbd1b0cc22141e8fcf40d9a007adf
Graham Watson <graham@phantomwatson.com>
2015-01-20 22:11:07 -0500

    Add complete_instructor_purchase()

--- app/Controller/StudentReviewModulesController.php
+++ app/Controller/StudentReviewModulesController.php
@@ -57,4 +57,59 @@ class StudentReviewModulesController extends AppController {
 		$this->set('retval', $retval);
 		$this->layout = 'json';
 	}
+
+	public function complete_instructor_purchase() {
+		$data = $_POST;
+
+		$instructor_id = isset($data['instructor_id']) ? $data['instructor_id'] : null;
+		$this->User->id = $instructor_id;
+		if (! $this->User->exists()) {
+			throw new NotFoundException('User #'.$instructor_id.' not found.');
+		}
+
+		$this->loadModel('Product');
+		$product_id = $this->Product->field(
+			'id',
+			array(
+				'name' => 'Student Review Module'
+			)
+		);
+		$this->Product->id = $product_id;
+		$quantity = $_POST['quantity'];
+		$total_cost = $this->Product->field('cost') * $quantity;
+		$instructor_email = $this->User->field('email');
+		$charge = array(
+			'amount' => $total_cost,
+			'stripeToken' => isset($data['token']) ? $data['token'] : null,
+			'description' => "Paying off $quantity used Student Review ".__n('Module', 'Modules', $quantity)." for $instructor_email (user #$instructor_id)"
+		);
+		$result = $this->Stripe->charge($charge);
+
+		if (is_array($result)) {
+			$this->loadModel('Purchase');
+			$this->Purchase->create(array(
+				'product_id' => $product_id,
+				'quantity' => $quantity,
+				'user_id' => $instructor_id,
+				'order_id' => $result['stripe_id']
+			));
+			if ($this->Purchase->save()) {
+				$retval = array('success' => true);
+			} else {
+				$retval = array(
+					'success' => false,
+					'message' => 'Payment was accepted, but there was an error making a record of this purchase.'
+				);
+				$this->response->statusCode('500');
+			}
+		} else {
+			$retval = array(
+				'success' => false,
+				'message' => $result
+			);
+			$this->response->statusCode('500');
+		}
+		$this->set('retval', $retval);
+		$this->layout = 'json';
+	}
 }
