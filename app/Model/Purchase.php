94d8ddc307da842c84c7e4b3467a133b9edf9329
Graham Watson <graham@phantomwatson.com>
2015-01-23 21:05:29 -0500

    Remove unused purchaseStudentReviewModule()

--- app/Model/Purchase.php
+++ app/Model/Purchase.php
@@ -57,57 +57,6 @@ class Purchase extends AppModel {
 		)
 	);
 
-	public function purchaseStudentReviewModule($seller_data, $order_id, $jwt_decoded) {
-		// Check for required sellerData
-		if (! isset($seller_data['instructor_id'])) {
-			throw new BadRequestException('Instructor ID missing');
-		} elseif (! $this->User->exists($seller_data['instructor_id'])) {
-			throw new BadRequestException('Instructor #'.$seller_data['instructor_id'].' not found');
-		} elseif (! isset($seller_data['product_id'])) {
-			throw new BadRequestException('Product ID missing');
-		} elseif (! isset($seller_data['quantity'])) {
-			throw new BadRequestException('Quantity missing');
-		}
-
-		// Record purchase
-		if (! isset($seller_data['user_id']) || empty($seller_data['user_id'])) {
-			$seller_data['user_id'] = null;
-			unset($this->validate['user_id']);
-		}
-		$this->create(array(
-			'quantity' => $seller_data['quantity'],
-			'product_id' => $seller_data['product_id'],
-			'user_id' => $seller_data['user_id'],
-			'order_id' => $order_id,
-			'jwt' => serialize($jwt_decoded)
-		));
-		if (! $this->save()) {
-			throw new InternalErrorException('Purchase could not be saved');
-		}
-		$purchase_id = $this->id;
-
-		// Apply this purchase to existing unpaid StudentReviewModule records
-		App::import('Model','StudentReviewModule');
-		$StudentReviewModule = new StudentReviewModule();
-		$unpaid_modules = $StudentReviewModule->getAwaitingPaymentList($seller_data['instructor_id']);
-		foreach ($unpaid_modules as $module_id => $module_course_id) {
-			$StudentReviewModule->id = $module_id;
-			$StudentReviewModule->saveField('purchase_id', $purchase_id);
-			$seller_data['quantity']--;
-		}
-
-		// Create StudentReviewModule records
-		for ($i = 1; $i <= $seller_data['quantity']; $i++) {
-			$StudentReviewModule->create(array(
-				'purchase_id' => $purchase_id,
-				'instructor_id' => $seller_data['instructor_id']
-			));
-			$StudentReviewModule->save();
-		}
-
-		return true;
-	}
-
 	public function purchaseClassroomModule($seller_data, $order_id, $jwt_decoded) {
 		// Check for required sellerData
 		if (! isset($seller_data['user_id'])) {
