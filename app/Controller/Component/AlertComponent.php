1b33e6d9c08a7b19bf400a56eabc22e587657cb0
Graham Watson <graham@phantomwatson.com>
2015-04-29 22:56:53 -0400

    Create refresh() and __refreshAdminTestimonials()

--- app/Controller/Component/AlertComponent.php
+++ app/Controller/Component/AlertComponent.php
@@ -12,7 +12,7 @@ class AlertComponent extends Component {
 		// Check for new alerts every ten minutes
 		$recheck = ! $this->Cookie->check('alerts_last_checked') || $this->Cookie->read('alerts_last_checked') < strtotime('10 minutes ago');
 		if ($recheck) {
-			$user_roles = $this->controller->__getUserRoles();
+			$user_roles = $this->controller->getUserRoles();
 			if (in_array('instructor', $user_roles)) {
 				$this->__setInstructorAlerts();
 			}
@@ -52,6 +52,19 @@ class AlertComponent extends Component {
 	}
 
 	protected function __setAdminAlerts() {
+		$this->adminTestimonials();
+	}
+
+	public function refresh($type) {
+		switch ($type) {
+			case 'admin testimonials':
+				$this->__refreshAdminTestimonials();
+				break;
+		}
+	}
+
+	protected function __refreshAdminTestimonials() {
+		$key = 'alerts.admin_testimonials';
 		$this->loadModel('Testimonial');
 		if ($this->Testimonial->approvalNeeded()) {
 			$url = Router::url(array(
@@ -59,7 +72,9 @@ class AlertComponent extends Component {
 				'action' => 'manage',
 				$this->params['prefix'] => false
 			));
-			$this->Cookie->write('alerts.admin_testimonials', 'Please <strong><a href="'.$url.'">approve or delete</a></strong> new testimonial(s).');
+			$this->Cookie->write($key, 'Please <strong><a href="'.$url.'">approve or delete</a></strong> new testimonial(s).');
+		} else {
+			$this->Cookie->delete($key);
 		}
 	}
 }
